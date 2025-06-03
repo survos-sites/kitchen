eff7decc2db4d0c8799b0b333d9d8574622b378e
Tac Tacelosky <tacman@gmail.com>
2025-06-03 08:04:47 -0400

    prep to deploy

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Survos\CommandBundle\SurvosCommandBundle::class => ['all' => true],
+    Bizkit\VersioningBundle\BizkitVersioningBundle::class => ['all' => true],
+    Survos\DeploymentBundle\SurvosDeploymentBundle::class => ['all' => true],
 ];
